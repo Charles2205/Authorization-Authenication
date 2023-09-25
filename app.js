ac92d9704934cbe38179a505fcce79d86159cf1a
Charles2205 <cewudzie0@gmail.com>
2023-09-25 11:59:56 +0000

    Added Db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotEnv = require('dotenv').config()
+const sequel = require('./dbConnect')
 const PORT = process.env.PORT || 2190
 
 const app = express()
@@ -12,6 +13,7 @@ app.get('/',(req,res)=>{
 
 
 const startServer =async()=>{
+    await sequel.authenticate()
     app.listen(PORT,()=>{
         console.log(`server is running on http://127.0.0.1:${PORT}`);
     })
